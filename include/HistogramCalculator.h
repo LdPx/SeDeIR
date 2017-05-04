a3fb3b8b3e4086f8d141a36cd993a607312402e0
forall <forall>
2017-05-04 16:02:09 +0200

    HistogramCalc

---
+++ include/HistogramCalculator.h
@@ -0,0 +1,18 @@
+#ifndef HISTOGRAMCALCULATOR_H
+#define HISTOGRAMCALCULATOR_H
+
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+class HistogramCalculator {
+public:
+    HistogramCalculator(cv::Vec3i nbins, cv::Vec3f range);
+    cv::Mat calc(cv::Mat hsv_img);
+
+    cv::Mat normalize(cv::Mat hist);
+protected:
+    cv::Vec3i nbins;
+    cv::Vec3f range;
+};
+
+#endif // HISTOGRAMCALCULATOR_H
---
+++ src/HistogramCalculator.cpp
@@ -0,0 +1,33 @@
+#include "../include/HistogramCalculator.h"
+
+using namespace std;
+using namespace cv;
+
+HistogramCalculator::HistogramCalculator(Vec3i nbins, Vec3f range){
+    this->nbins = nbins;
+    this->range = range;
+}
+
+Mat HistogramCalculator::calc(Mat hsv_img){
+    vector<Mat> img_vec = {hsv_img};
+    vector<int> use_all_channels = {0, 1, 2};
+    Mat res;
+    vector<int> nbins_vec = {nbins[0], nbins[1], nbins[2]};
+    vector<float> ranges_vec = {0, range[0], 0, range[1], 0, range[2]};
+    calcHist(img_vec, use_all_channels, Mat(), res, nbins_vec, ranges_vec);
+
+    //dimensions of mat with dim 3+
+    //cout << "(" << res.size[0] << "," << res.size[1] << "," << res.size[2] << ")" << endl;
+
+    // convert 3D-histogram to 1D-histogram
+    // of size 1 x (number of hist-elements) (row vector)
+    //Mat mat1d(1, res.total(), res.type(), (void*)res.data);
+    //return mat1d.clone();
+    return res;
+}
+
+Mat HistogramCalculator::normalize(Mat hist){
+    Mat norm_hist;
+    cv::normalize(hist, norm_hist, 1, 0, cv::NORM_L1);
+    return norm_hist;
+}
