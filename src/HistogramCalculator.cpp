8cddafb491e8cac8327c6dbdfe9f0ef6a8b94aa7
LdPx <til_schramke@yahoo.de>
2017-05-11 08:12:52 +0200

    The New Line Order

--- src/HistogramCalculator.cpp
+++ src/HistogramCalculator.cpp
@@ -3,21 +3,23 @@
 using namespace std;
 using namespace cv;
 
-Mat HistogramCalculator::calc(Mat hsv_img){
-	int hbins = 180;
-	int histSize[] = { hbins };
-	// hue varies from 0 to 179
-	float hranges[] = { 0, 180 };
-	const float* ranges[] = { hranges };
-	MatND hist;
-	int channels[] = { 0 };
-	calcHist(&hsv_img, 1, channels, Mat(), hist, 1, histSize, ranges, true, false);
-
-    return hist;
-}
-
-Mat HistogramCalculator::normalize(Mat hist){
-    Mat norm_hist;
-    cv::normalize(hist, norm_hist, 1, 0, cv::NORM_L1);
-    return norm_hist;
-}
+// Erneuern der Zeilennummerierung.
+// Hier fängt der Kot an.
+/* 1*/	Mat HistogramCalculator::calc(Mat hsv_img){
+/* 2*/	int hbins = 180;
+/* 3*/	int histSize[] = { hbins };
+/* 4*/	// hue varies from 0 to 179
+/* 5*/	float hranges[] = { 0, 180 };
+/* 6*/	const float* ranges[] = { hranges };
+/* 7*/	MatND hist;
+/* 8*/	int channels[] = { 0 };
+/* 9*/	calcHist(&hsv_img, 1, channels, Mat(), hist, 1, histSize, ranges, true, false);
+/*10*/
+/*11*/	return hist;
+/*12*/	}
+/*13*/
+/*14*/	Mat HistogramCalculator::normalize(Mat hist){
+/*15*/	Mat norm_hist;
+/*16*/	cv::normalize(hist, norm_hist, 1, 0, cv::NORM_L1);
+/*17*/	return norm_hist;
+/*18*/}
